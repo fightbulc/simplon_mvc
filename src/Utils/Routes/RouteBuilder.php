8f33ce501a8753baf142c6c06e9f52631e47464f
Tino Ehrich <tino@bigpun.me>
2015-11-16 17:05:45 +0100

    added domain for route building

--- src/Utils/Routes/RouteBuilder.php
+++ src/Utils/Routes/RouteBuilder.php
@@ -11,17 +11,24 @@ use Simplon\Mvc\Utils\CastAway;
 abstract class RouteBuilder
 {
     /**
+     * @param string $domain
      * @param string|array $paths
      * @param array $params
      *
      * @return string
      */
-    protected static function buildUri($paths, array $params = [])
+    protected static function buildUri($domain, $paths, array $params = [])
     {
         if (is_array($paths) === false)
         {
             $paths = [$paths];
         }
+
+        // remove leading slash
+        $paths[0] = trim($paths[0], '/');
+
+        // trim url and lets adopt protocol
+        array_unshift($paths, '//' . CastAway::trimUrl($domain));
 
         return CastAway::pathsToUriString($paths, $params);
     }
