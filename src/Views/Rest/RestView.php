f34d517c30a2e2de637a45b9febed5895f00d808
Tino Ehrich <tino@bigpun.me>
2015-12-22 14:55:20 +0100

    added locale methods

--- src/Views/Rest/RestView.php
+++ src/Views/Rest/RestView.php
@@ -2,6 +2,7 @@
 
 namespace Simplon\Mvc\Views\Rest;
 
+use Simplon\Locale\Locale;
 use Simplon\Mvc\Interfaces\RestViewInterface;
 
 /**
@@ -11,9 +12,42 @@ use Simplon\Mvc\Interfaces\RestViewInterface;
 abstract class RestView implements RestViewInterface
 {
     /**
+     * @var Locale
+     */
+    private $locale;
+
+    /**
      * @var array
      */
     protected $result = [];
+
+    /**
+     * @return Locale
+     */
+    public function getLocale()
+    {
+        return $this->locale;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLocaleCode()
+    {
+        return $this->locale->getCurrentLocale();
+    }
+
+    /**
+     * @param Locale $locale
+     *
+     * @return RestViewInterface
+     */
+    public function setLocale(Locale $locale)
+    {
+        $this->locale = $locale;
+
+        return $this;
+    }
 
     /**
      * @return array
